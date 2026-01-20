168216adb20e842795c903d0a7bbe7923460b964
sahhoutiamine <midoriaduko@gmail.com>
2026-01-20 10:32:04 +0100

    create database class for connection

---
+++ app/Core/Database.php
@@ -0,0 +1,18 @@
+<?php 
+
+
+namespace app\Core;
+
+use PDO;
+use PDOException;
+class Database {
+    private $host = "localhost";
+    private $dbname = "taskydb";
+    private $username = "root";
+    private $pw = "";
+    private $pdo;
+    private static $instance = null;
+
+
+    
+}
