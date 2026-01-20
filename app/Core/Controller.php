4e48cd83adda772d11453da6a779d75fb77cac19
sahhoutiamine <midoriaduko@gmail.com>
2026-01-20 12:47:24 +0100

    add construct to the controller

---
+++ app/Core/Controller.php
@@ -0,0 +1,20 @@
+<?php
+
+use twig\Loader\FileSystemLoader;
+use twig\Environment;
+
+
+class Controller {
+    protected $twig;
+
+    public function __construct () {
+        $loader = new FileSystemLoader(__DIR__. "/../Views");
+        $this->twig = new Environment($loader, [
+            'cache' => false,
+            'dubeg' => true
+        ]);
+
+        $this->twig->addGlobale('session', $_SESSION);
+    }
+
+}
