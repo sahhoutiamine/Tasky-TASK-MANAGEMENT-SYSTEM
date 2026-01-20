ee8ccd97c9113513ba18243104949e2cdd1a55c7
sahhoutiamine <midoriaduko@gmail.com>
2026-01-20 12:51:20 +0100

    add render method

--- app/Core/Controller.php
+++ app/Core/Controller.php
@@ -17,4 +17,8 @@ class Controller {
         $this->twig->addGlobale('session', $_SESSION);
     }
 
+    protected function render($view, $data) {
+        echo $this->twig->render($viewm $data);
+    }
+
 }
